0821d4e173c9271f0d7db1a8b536669d00a0b25e
Katrina Pritchard <pritckat@gmail.com>
2018-10-14 02:09:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function produceDrivingRange(range) {
   return function(start, stop) {
-    return (range > (start - stop) ? true : `${range - (start - stop)}`);
+    return (range > (start - stop) ? true : `${range}`);
   }
 }
