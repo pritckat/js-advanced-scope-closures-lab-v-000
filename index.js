4a994518a8fe767a1530ac495a4c61357d55f440
Katrina Pritchard <pritckat@gmail.com>
2018-10-14 02:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function produceDrivingRange(range) {
   return function(start, stop) {
     startdig = start.slice(0, start.length - 2)
     endig = stop.slice(0, stop.length - 2)
-    return (range > Math.abs(startdig - endig) ? true : `${range - Math.abs(startdig - endig)} blocks out of range`);
+    return (range > Math.abs(startdig - endig) ? true : `${Math.abs(range - Math.abs(startdig - endig))} blocks out of range`);
   }
 }
