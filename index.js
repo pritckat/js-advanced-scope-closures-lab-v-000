1a0415be2bdfec343cfd4bddf05137078a554f24
Katrina Pritchard <pritckat@gmail.com>
2018-10-14 02:17:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function produceDrivingRange(range) {
     let startdig = start.slice(0, start.length - 2)
     let endig = stop.slice(0, stop.length - 2)
     let difference = Math.abs(endig - startdig)
-    return (range > difference ? `Within range by ${Math.abs(range - difference)}` : `${Math.abs(range - difference)} blocks out of range`);
+    return (range > difference ? `within range by ${Math.abs(range - difference)}` : `${Math.abs(range - difference)} blocks out of range`);
   }
 }
